3a65b9eb4c8295e633c84140ca63fe7c515482f6
AugustoV2 <augustovarghese@gmail.com>
2024-08-19 06:38:29 +0530

    new aaa

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -30,14 +30,14 @@ const Home = () => {
                 <TypeAnimation
                   sequence={[
 
-                    'Citize Alert',
+                    'Citizen Alert',
                     2000,
 
                     'CrowdIN',
                     1000,
-                    'friewnd',
+                    'Partner',
                     1000,
-                    'AAAAAAA',
+                    'Helper',
                     1000
                   ]}
                   wrapper="span"
@@ -49,8 +49,8 @@ const Home = () => {
             </div>
 
 
-            <p className='mb-8 text-gray-500'>
-           chakkaman
+            <p className='mb-8 pt-5 text-gray-500'>
+            Welcome to Crowdin, a revolutionary alert system designed for today's bustling urban environments. In a world where traditional methods often fall short, Crowdin empowers communities with real-time, citizen-driven alerts. Our cutting-edge technology ensures that you receive timely and accurate information during critical events, enhancing public safety and convenience. Join us in transforming how we respond to emergencies and stay informed.
             </p>
 
           </div>
